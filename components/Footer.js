eccf840786a37f3ac37f16e02c38c9a61ea408a6
Horma-mohamed <96146244+Horma-mohamed@users.noreply.github.com>
2022-10-30 18:40:02 +0100

    first commit

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function  Footer(){
         <div className=' w-full  bg-gray-40 absolute bottom-0 bg-gray-20 h-20 dark:bg-gray-800 ' >
             <Flex h='100%' justifyContent='Center' alignItems='center'   >
                 <Box display='flex' justifyContent='center' >
-                    <Text fontSize='20px' className='text-gray-800 dark:text-gray-100' >Builded with <span  ><GiHeartOrgan className='heart relative  inline text-red-700 dark:text-red-600' /></span> by me</Text>
+                    <Text fontSize='20px' className='text-gray-800 dark:text-gray-100' >Made with <span  ><GiHeartOrgan className='heart relative  inline text-red-700 dark:text-red-600' /></span> by me</Text>
                 </Box>
             </Flex>
         </div>
